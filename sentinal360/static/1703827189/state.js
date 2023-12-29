6d46e435ff235cf133f3eb0d22ca2e90f24ec0bc
Ejaz Ansari <ejaz.portal@gmail.com>
2023-12-29 00:22:33 -0500

    "LocalPublish2023-12-29"

--- sentinal360/static/1703827189/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002Fsentinal360\u002Fstatic\u002F1703827189",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002Fsentinal360\u002F",cdnURL:a}}}}(null,"\u002F"));
